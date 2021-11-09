4558361c6a9c5a37bde9e9456c2f6f232f4cb019
tlatmdxo123 <tmdxosmswkd@gmail.com>
2021-11-09 22:01:30 +0900

    add: 페이지 라우팅 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
+import { PageContainer } from './pages/Container';
+import Main from './pages/Main';
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+        <PageContainer className="App">
+            <Switch>
+              <Route path='/' exact>
+                <Main/>
+              </Route>
+            </Switch>
+        </PageContainer>
+    </Router>
+    
   );
 }
 
